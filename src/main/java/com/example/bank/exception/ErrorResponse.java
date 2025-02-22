d0a54f68fcac91d272f61a590756c4247804305e
houyuxin <houyuxin@mininglamp.com>
2025-02-22 11:57:51 +0800

    style: use lombok

--- src/main/java/com/example/bank/exception/ErrorResponse.java
+++ src/main/java/com/example/bank/exception/ErrorResponse.java
@@ -1,28 +1,11 @@
 package com.example.bank.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ErrorResponse {
     private int status;
     private String message;
-
-    public ErrorResponse(int status, String message) {
-        this.status = status;
-        this.message = message;
-    }
-
-    // Getters and Setters
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
